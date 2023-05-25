546d4acc73e8757fcee8256c5b73e36b8c55bccb
daaajuol <orkoniitty@hotmail.com>
2023-05-25 17:13:41 +0300

    Add translations

--- timApp/modules/cs/js/editor/math-editor/formula-field.component.ts
+++ timApp/modules/cs/js/editor/math-editor/formula-field.component.ts
@@ -56,8 +56,8 @@ const ERROR_MESSAGE_DELAY = 1000;
     template: `
         <div class="formula-field" [class.active-field]="isActive">
             <div class="formula-field-undoredo btn-group btn-group-xs" *ngIf="isActive">
-                <button class="btn btn-default" (click)="handleUndo()" i18n title="Ctrl+z"><b>Undo</b></button>
-                <button class="btn btn-default" (click)="handleRedo()" i18n title="Ctrl+y"><b>Redo</b></button>
+                <button class="btn btn-default" (click)="handleUndo()" title="Ctrl+z"><b i18n>Undo </b></button>
+                <button class="btn btn-default" (click)="handleRedo()" title="Ctrl+y"><b i18n>Redo </b></button>
             </div>
             <div class="input-container" #inputContainer>
                 <span
